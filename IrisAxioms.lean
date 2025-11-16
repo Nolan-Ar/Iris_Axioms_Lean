f4147f00c60352fa82f866c0462f5ecabe5fc542
Nolan <arnaultnolan@gmail.com>
2025-11-16 23:00:50 +0100

    frist

---
+++ IrisAxioms.lean
@@ -0,0 +1,9 @@
+-- This module serves as the root of the `IrisAxioms` library.
+-- Import modules here that should be built as part of the library.
+import «IrisAxioms».iris_axioms
+import «IrisAxioms».validation_correctifs
+import «IrisAxioms».iris_brique
+import «IrisAxioms».iris_axioms_extended
+import «IrisAxioms».iris_exemples_numeriques
+import «IrisAxioms».iris_theoremes_extended
+import «IrisAxioms».iris_validation_complete
---
+++ Main.lean
@@ -0,0 +1,10 @@
+import IrisAxioms.iris_axioms
+import IrisAxioms.validation_correctifs
+import IrisAxioms.iris_brique
+import IrisAxioms.iris_axioms_extended
+import IrisAxioms.iris_exemples_numeriques
+import IrisAxioms.iris_theoremes_extended
+import IrisAxioms.iris_validation_complete
+
+def main : IO Unit := do
+  IO.println "IRIS compilé. Toutes les preuves ont été vérifiées "
