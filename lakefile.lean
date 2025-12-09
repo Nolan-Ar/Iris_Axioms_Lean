72f0da81bf35a0afbe5c390f83bb2e478417e775
Nolan-Ar <arnaultnolan.pro@gmail.com>
2025-12-09 20:21:38 +0100

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -8,7 +8,7 @@ lean_lib «IrisAxioms» where
   -- add library configuration options here
 
 @[default_target]
-lean_exe «Irisaxioms» where
+lean_exe «irisaxioms» where
   root := `Main
   -- Enables the use of the Lean interpreter by the executable (e.g.,
   -- `runFrontend`) at the expense of increased binary size on Linux.
